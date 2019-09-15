386d4133ccae2e5e96aa1002a0c9fda24f847adf
Evette Telyas <etelyas@gmail.com>
2019-09-15 14:40:16 -0600

    update orders table to push to heroku

--- db/migrate/20190824200120_create_orders.rb
+++ db/migrate/20190824200120_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.string :name
-      t.string :street_address
+      t.string :address
       t.string :city
       t.string :state
       t.integer :zip
--- db/migrate/20190914153309_remove_name_from_orders.rb
+++ db/migrate/20190914153309_remove_name_from_orders.rb
@@ -1,7 +1,7 @@
 class RemoveNameFromOrders < ActiveRecord::Migration[5.1]
   def change
     remove_column :orders, :name, :string
-    remove_column :orders, :street_address, :string
+    remove_column :orders, :address, :string
     remove_column :orders, :city, :string
     remove_column :orders, :state, :string
     remove_column :orders, :zip, :string
